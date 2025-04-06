e85c0ace0b623011acb6c8737e61bb7804c17e0b
Nicolas A Perez <anicolaspp@gmail.com>
2025-04-06 13:44:32 -0700

    Using pieces instead of points. This allows me to draw any pieces as a series of points.

--- main.go
+++ main.go
@@ -17,19 +17,72 @@ func main() {
 	}
 }
 
+// piece is a game piece that can be moved across the board until it is
+// emprinted which then becomes part of the board.
+type piece struct {
+	points []*point
+}
+
+// canMoveDown returns true if the piece can be moved down based on the current
+// configuration of the board.
+func (p *piece) canMoveDown(b board) bool {
+	for _, point := range p.points {
+		if !point.canMoveDown(b) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (p *piece) moveDown() {
+	for _, point := range p.points {
+		point.x++
+	}
+}
+
+// isIn return true if the given point is part of the piece.
+func (p *piece) isIn(point point) bool {
+	for _, pp := range p.points {
+		if pp.eq(point) {
+			return true
+		}
+	}
+	return false
+}
+
 type point struct {
 	x, y int
+}
+
+// eq returns true if two point has the same coordinates.
+func (p point) eq(other point) bool {
+	return p.x == other.x && p.y == other.y
+}
+
+// canMoveDown return true if the point can be moved down.
+func (p point) canMoveDown(b board) bool {
+	if p.y+1 < len(b.m) && b.m[p.y+1][p.x] != 1 { // Move down allowed.
+		return true
+	}
+
+	return false
 }
 
 type model struct {
 	board *board
 
 	pos point
+
+	// The current pience being moved in the board.
+	piece piece
 }
 
 func initialModel() model {
 	return model{
 		board: initBoard(),
+
+		piece: pickPiece(),
 	}
 }
 
@@ -66,13 +119,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		// The "down" and "j" keys move the cursor down
 		case "down", "j":
-			if m.pos.y+1 < len(m.board.m) && m.board.m[m.pos.y+1][m.pos.x] != 1 {
-				m.pos.y++
+			if m.piece.canMoveDown(*m.board) {
+				m.piece.moveDown()
 				break
 			}
 
-			m.board.m[m.pos.y][m.pos.x] = 1
-			m.pos = point{}
+			m.board.emprint(m.piece)
+			m.piece = pickPiece()
+
+			// if m.pos.y+1 < len(m.board.m) && m.board.m[m.pos.y+1][m.pos.x] != 1 {
+			// 	m.pos.y++
+			// 	break
+			// }
+
+			// m.board.m[m.pos.y][m.pos.x] = 1
+			// m.pos = point{}
 		}
 	}
 
@@ -83,6 +144,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 type board struct {
 	m [][]int
+}
+
+// emprint writes 1' in the board as the points indicate.
+func (b *board) emprint(piece piece) {
+	for _, p := range piece.points {
+		b.m[p.y][p.x] = 1
+	}
 }
 
 func (b *board) P(y, x int) string {
@@ -118,14 +186,32 @@ func (m model) View() string {
 	for i := range 33 {
 		var row string
 		for j := range 16 {
-			if i == m.pos.y && j == m.pos.x {
+			if m.piece.isIn(point{i, j}) {
 				row += "1"
 			} else {
 				row += fmt.Sprintf("%v", m.board.m[i][j])
 			}
+
+			// if i == m.pos.y && j == m.pos.x {
+			// 	row += "1"
+			// } else {
+			// 	row += fmt.Sprintf("%v", m.board.m[i][j])
+			// }
 		}
 		board += row + "\n"
 	}
 
 	return board
+}
+
+// gamePieces defines the possible pieces that the game can show.
+var gamePieces = []piece{
+	{
+		points: []*point{{x: 0, y: 0}},
+	},
+}
+
+// pickPiece returns a random piece for the game.
+func pickPiece() piece {
+	return gamePieces[0]
 }
